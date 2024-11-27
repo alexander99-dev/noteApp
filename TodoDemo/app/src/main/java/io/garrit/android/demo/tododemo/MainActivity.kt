b7e3c643544ffc1724454f54383059f8bcaf2b60
alexander99-dev <alexander.olsson532@gmail.com>
2024-11-27 13:21:21 +0100

    new changes

--- TodoDemo/app/src/main/java/io/garrit/android/demo/tododemo/MainActivity.kt
+++ TodoDemo/app/src/main/java/io/garrit/android/demo/tododemo/MainActivity.kt
@@ -1,24 +1,16 @@
 package io.garrit.android.demo.tododemo
 
 import android.os.Bundle
-import android.widget.EditText
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Checkbox
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -27,168 +19,202 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import io.garrit.android.demo.tododemo.ui.theme.TodoDemoTheme
-import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
-import java.util.UUID
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.*
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
-data class Task(
-    val id: String = UUID.randomUUID().toString(),
-    val title: String,
-    var isChecked: MutableState<Boolean> = mutableStateOf(false)
+data class Note(
+    val id: Int,
+    var title: String,
+    var subtitle: String,
+    val check: MutableState<Boolean> = mutableStateOf(false)
 )
 
-var i = 0
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            val list = remember {
-                mutableStateListOf(Task(title = "Hello"), Task(title = "World"))
-            }
+            NoteApp()
+        }
+    }
+}
 
-            TodoDemoTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    MainScreen(list = list)
+@Composable
+fun NoteApp() {
+    val navController = rememberNavController()
+    val ListNote = remember { mutableStateListOf<Note>() }
+
+    NavHost(navController = navController, startDestination = "todoList") {
+        composable("todoList") { ListScreen(navController, ListNote) }
+        composable("addTodo") { AddNoteScreen(navController, ListNote) }
+        composable("editTodo/{itemId}") { backStackEntry ->
+            val itemId = backStackEntry.arguments?.getString("itemId")?.toIntOrNull()
+            val todoItem = ListNote.find { it.id == itemId }
+            todoItem?.let { EditNoteScreen(navController, it) }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ListScreen(navController: NavController, todoList: MutableList<Note>) {
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text("Notes app") })
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = { navController.navigate("addTodo") }) {
+                Icon(Icons.Filled.Add, contentDescription = "Add")
+            }
+        }
+    ) { padding ->
+        LazyColumn(modifier = Modifier.padding(padding)) {
+            items(todoList) { item ->
+                ListItem(
+                    leadingContent = {
+                        Checkbox(
+                            checked = item.check.value,
+                            onCheckedChange = {
+                                item.check.value = !item.check.value
+                            })},
+                    headlineContent = { Text(item.title) },
+                    supportingContent = { Text(item.subtitle)},
+                    trailingContent = {
+                        Row {
+                            IconButton(
+                                onClick = { navController.navigate("editTodo/${item.id}") }
+                            ) {
+                                Icon(Icons.Filled.Edit, contentDescription = "Edit note")
+                            }
+                            IconButton(
+                                onClick = { todoList.remove(item) }
+                            ) {
+                                Icon(Icons.Filled.Delete, contentDescription = "Delete Note")
+                            }
+                        }
+                    }
+                )
+            }
+        }
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AddNoteScreen(navController: NavController, todoList: MutableList<Note>) {
+    var title by remember { mutableStateOf("") }
+    var subtitle by remember { mutableStateOf("") }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Add Todo") },
+                navigationIcon = {
+                    IconButton(
+                        onClick = {
+                            if (title.isNotBlank() && subtitle.isNotBlank()) {
+                                todoList.add(Note(id = todoList.size, title = title, subtitle = subtitle))
+                                navController.popBackStack()
+                            }
+                        }) {
+
+                    }
                 }
-            }
-        }
-    }
-}
-
-@Composable
-fun MainScreen(list: MutableList<Task>, modifier: Modifier = Modifier) {
-    Column(
-        modifier = modifier
-            .fillMaxSize()
-    ) {
-        TextInputView(list = list)
-        ListView(list = list)
-    }
-}
-
-
-
-@Composable
-fun TextInputView(list: MutableList<Task>) {
-    var title by rememberSaveable {
-        mutableStateOf("")
-    }
-    var mainText by rememberSaveable {
-        mutableStateOf("")
-    }
-    var additionalInfo by rememberSaveable {
-        mutableStateOf("")
-    }
-
-
-    val inputList:List<String> = listOf("add title to note", "add text to note", "any additional info")
-
-
-
-    Column(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        OutlinedTextField(value = title, onValueChange = {
-            title = it
-        }, label = {Text("Title on note ")}
-        )
-        OutlinedTextField(value = mainText, onValueChange = {
-            mainText = it
-        }, label = {Text("MainText on note  ")}
-        )
-        OutlinedTextField(value = additionalInfo, onValueChange = {
-            additionalInfo = it
-        }, label = {Text("AdditionalInfo on note")}
-        )
-        Button(onClick = {
-            if(title.length >= 3 && title.length <= 50 && mainText.length < 120){
-                list.add(Task(title = "hello"))
-                title = ""
-                mainText = ""
-                additionalInfo = ""
-            }
-
-
-        }) {
-            Text("Add")
-        }
-    }
-}
-
-@Composable
-fun ListView(list: List<Task>) {
-    LazyColumn {
-        items(list) { task ->
-            RowView(task)
-        }
-    }
-}
-
-@Composable
-fun RowView(task: Task) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth(),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Checkbox(
-            checked = task.isChecked.value,
-            onCheckedChange = {
-                task.isChecked.value = !task.isChecked.value
-            }
-        )
-        Text(task.title)
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun RowViewPreview() {
-
-
-
-    Card(
-        colors = CardDefaults.cardColors(
-            containerColor = MaterialTheme.colorScheme.surfaceVariant,
-        ),
-        modifier = Modifier
-            .size(width = 240.dp, height = 100.dp)
-    ) {
-        Text(text = "m",
-
             )
-        Text(
-            text = "Ffasfdasfdsafsa",
-            textAlign = TextAlign.Center,
-            modifier = Modifier.fillMaxWidth()
-        )
-        Button() { }
-    }
-
-
-
-
-    /*
-    val list = mutableStateListOf(Task(title =  "hello"), Task(title = " tjao"))
-    TodoDemoTheme {
-        LazyColumn() {
-            items(list) { task ->
-                RowView(task)
+        }
+    ) { padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding),
+//            horizontalAlignment = Alignment.CenterHorizontally,
+//            verticalArrangement = Arrangement.Center
+        ) {
+            TextField(
+                value = title,
+                onValueChange = { title = it },
+                label = { Text("Todo") }
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            TextField(
+                value = subtitle,
+                onValueChange = { subtitle = it },
+                label = { Text("Details") }
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(onClick = {
+                if (title.isNotBlank() && subtitle.isNotBlank()) {
+                    todoList.add(Note(id = todoList.size, title = title, subtitle = subtitle))
+                    navController.popBackStack()
+                }
+            }) {
+                Text("Add Todo")
             }
         }
-    }*/
+    }
+}
+
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditNoteScreen(navController: NavController, todoItem: Note) {
+    var title by remember { mutableStateOf(todoItem.title) }
+    var subtitle by remember { mutableStateOf(todoItem.subtitle) }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Edit Todo") },
+                navigationIcon = {
+                    IconButton(onClick = { navController.popBackStack() }) {
+
+                    }
+                }
+            )
+        }
+    ) { padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding),
+//            horizontalAlignment = Alignment.CenterHorizontally,
+//            verticalArrangement = Arrangement.Center
+        ) {
+            TextField(
+                value = title,
+                onValueChange = { title = it },
+                label = { Text("Title For the Note") }
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            TextField(
+                value = subtitle,
+                onValueChange = { subtitle = it },
+                label = { Text("Edit text") }
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(onClick = {
+                if (title.isNotBlank() && subtitle.isNotBlank()) {
+                    todoItem.title = title
+                    todoItem.subtitle = subtitle
+                    navController.popBackStack()
+                }
+            }) {
+                Text("Save Todo")
+            }
+        }
+    }
 }
